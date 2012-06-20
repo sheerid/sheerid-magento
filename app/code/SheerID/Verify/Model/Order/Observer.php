c226b8f784ba47284f794a4516b9fd6069be6532
Alex Boone <alex@sheerid.com>
2012-06-20 12:25:33 -0400

    Conversion stuff

--- app/code/SheerID/Verify/Model/Order/Observer.php
+++ app/code/SheerID/Verify/Model/Order/Observer.php
@@ -4,20 +4,32 @@ class SheerID_Verify_Model_Order_Observer
 {
 	public function on_sales_order_invoice_pay($observer) {
 		$invoice = $observer->getEvent()->getInvoice();
-	    if ($invoice->getState() == Mage_Sales_Model_Order_Invoice::STATE_PAID) {
-			//$this->track_conversion($invoice);
-	    }
+		$this->orderUpdated();
 	    return $this;
 	}
 	
 	public function on_sales_order_save_commit_after($observer) {
 	    $order = $observer->getOrder();
 	    if ($order->getState() == Mage_Sales_Model_Order::STATE_COMPLETE) {
-	    	$this->track_conversion($order);
+	    	$this->orderUpdated();
 	    }
+		return $this;
 	}
 	
-    private function track_conversion($orderOrInvoice) {
+	private function orderUpdated($order) {
+		if (!$order || !$order->getSheeridRequestId()) {
+			return;
+		}
+		if ($order->getState() == Mage_Sales_Model_Order::STATE_COMPLETE) {
+			$this->trackConversion($order);
+		}
+	}
+	
+    private function linkOrder($order) {
+		// TODO: update SheerID request with conversion info: total spend, discount, etc.
+	}
+	
+	private function trackConversion($order) {
 		// TODO: update SheerID request with conversion info: total spend, discount, etc.
 	}
 }
