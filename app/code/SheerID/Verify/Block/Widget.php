5c0d98bf02a5b39d66c97b209c4cf009e453b17b
EC2 Default User <ec2-user@services-sandbox.sheerid.com>
2012-07-24 22:07:05 +0000

    Default to is_conditional = true for widget

--- app/code/SheerID/Verify/Block/Widget.php
+++ app/code/SheerID/Verify/Block/Widget.php
@@ -4,6 +4,7 @@ class SheerID_Verify_Block_Widget extends Mage_Core_Block_Template
     protected function _construct()
     {
         parent::_construct();
+        $this->setIsConditional("true");
         $this->setTemplate('verify/verify-widget.phtml');
     }
 
