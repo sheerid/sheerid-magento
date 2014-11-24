8e76eb4a4662af80b2aad937b49b4960966ca492
Alex Boone <alex@sheerid.com>
2014-11-24 12:02:40 -0500

    Remove code related to retired 1.x features [#78298940]

--- app/code/SheerID/Verify/Helper/Data.php
+++ app/code/SheerID/Verify/Helper/Data.php
@@ -2,106 +2,6 @@
 class SheerID_Verify_Helper_Data extends Mage_Core_Helper_Abstract
 {
 
-	private static $NOTIFIER_TAG = "magento";
-
-	public function handleVerifyPost($request, $response, $quote) {
-		if ($request->isPost()) {
-			$post_data = $request->getPost();
-			$verify = $post_data['verify'];
-
-			$organizationId = $verify['organizationId'];
-			$dob = $this->readDate($verify, 'BIRTH_DATE');
-			$ssd = $this->readDate($verify, 'STATUS_START_DATE');
-
-			$ba = $quote->getBillingAddress();
-			if ($ba) {
-				$firstName = $ba->getFirstname();
-				$lastName = $ba->getLastname();
-				$postalCode = $ba->getPostcode();
-				$email = $ba->getEmail();
-			}
-			
-			$ALLOW_NAME = true;
-			if ($ALLOW_NAME && $verify['FIRST_NAME']) {
-				$firstName = $verify['FIRST_NAME'];
-			}
-			if ($ALLOW_NAME && $verify['LAST_NAME']) {
-				$lastName = $verify['LAST_NAME'];
-			}
-
-			$data = array();
-			$data["FIRST_NAME"] = $firstName;
-			$data["LAST_NAME"] = $lastName;
-			if (strlen($dob) == 10) {
-				$data["BIRTH_DATE"] = $dob;
-			}
-			if (strlen($ssd) == 10) {
-				$data["STATUS_START_DATE"] = $ssd;
-			}
-			$data["ID_NUMBER"] = $verify['ID_NUMBER'];
-
-			if ($verify['POSTAL_CODE']) {
-				$data["POSTAL_CODE"] = $verify['POSTAL_CODE'];
-			} else {
-				$data['POSTAL_CODE'] = $postalCode;
-			}
-			if ($verify['EMAIL']) {
-				$data['EMAIL'] = $verify['EMAIL'];
-			} else if ($email) {
-				$data['EMAIL'] = $email;
-			}
-
-			if ($verify['templateId']) {
-				$data['templateId'] = $verify['templateId'];
-			}
-			if ($verify['affiliation_types']) {
-				//TODO: use config object
-				$data["_affiliationTypes"] = $verify['affiliation_types'];
-			}
-
-			if (!is_numeric($organizationId)) {
-				$data['organizationName'] = $organizationId;
-				$organizationId = null;
-			}
-
-			$data[':ipv4Address'] = $_SERVER['REMOTE_ADDR'];
-
-			$SheerID = Mage::helper('sheerid_verify/rest')->getService();
-			
-			$result = array();
-			
-			if (!$SheerID) {
-				$result["error"] = true;
-				$result['message'] = "No access token";
-				return $result;
-			}
-
-			try {
-				$data = $this->filterEmptyFields($data);
-				$resp = $SheerID->verify($data, $organizationId);
-				$result["result"] = $resp->result;
-				$result["requestId"] = $resp->requestId;
-				if (!$resp->result) {
-					if ($resp->requestId && $this->allowSendEmail()) {
-						// provide a success URL so we know where to send users after asset review success
-						$SheerID->updateMetadata($resp->requestId, array('successUrl' => $this->getSuccessUrl($resp->requestId)));
-					}
-					$errors = $resp->errors;
-					if (count($errors) == 1 && $errors[0]->code == 39 && $this->allowUploads()) {
-						$result["awaiting_upload"] = true;
-					}
-				}
-			} catch (Exception $e) {
-				$result["error"] = true;
-				$result['message'] = $e->getMessage();
-			}
-
-			$this->saveResponseToQuote($quote, $resp);
-
-			return $result;
-		}
-	}
-
 	public function saveResponseToQuote($quote, $resp) {
 		if ($quote && $resp) {
 			$affs = array();
@@ -167,30 +67,6 @@ class SheerID_Verify_Helper_Data extends Mage_Core_Helper_Abstract
 		return false;
 	}
 
-	public function shouldShowInCheckout() {
-		$show_in_checkout = $this->getSetting("show_in_checkout");
-		$cookie_name = $this->getSetting("show_in_checkout_cookie_name");
-		$quote = Mage::getSingleton('checkout/cart')->getQuote();
-
-		if ('false' == $show_in_checkout) {
-			return false;
-		} else {
-			$affiliation_types = $this->getCheckoutAffiliationTypes();
-			return count(array_intersect($affiliation_types, $this->getSheeridAffiliations($quote))) == 0;
-		}
-	}
-
-	public function getCheckoutAffiliationTypes() {
-		$affiliation_types_list = '';
-		$show_in_checkout = $this->getSetting("show_in_checkout");
-		if ('cookie' == $show_in_checkout) {
-			$affiliation_types_list = $_COOKIE[$this->getSetting("show_in_checkout_cookie_name")];
-		} else if ('true' == $show_in_checkout) {
-			$affiliation_types_list = $this->getSetting("show_in_checkout_affiliation_types");
-		}
-		return array_filter(explode(',', $affiliation_types_list));
-	}
-
 	public function getCurrentQuote($create=true) {
 		$quote = Mage::getSingleton('checkout/cart')->getQuote();
 		$session = Mage::getSingleton('checkout/session');
@@ -206,28 +82,6 @@ class SheerID_Verify_Helper_Data extends Mage_Core_Helper_Abstract
 		$hostname = $sandbox ? 'verify-demo.sheerid.com' : 'verify.sheerid.com';
 		return "https://$hostname/verify/$templateId/";
 	}
-
-	public function getFieldLabel($key) {
-		$lbl = strtolower(str_replace("_", " ", $key));
-		return $this->__(preg_replace_callback("/\b([a-z])/", array($this, 'titleCaseReplace'), $lbl));
-	}
-
-	public function getOrganizationType($affiliation_types) {
-		return $SheerID = Mage::helper('sheerid_verify/rest')->getService()->getOrganizationType($affiliation_types);
-	}
-
-	public function getOrganizationLabel($orgType) {
-		if ("UNIVERSITY" == $orgType) {
-			return "School";
-		} else if ('MILITARY' == $orgType) {
-			return "Branch";
-		}
-		return "Organization";
-	}
-
-	private function titleCaseReplace($m) {
-		return strtoupper($m[1]);
-	}
 	
 	public function getSetting($key) {
 		return Mage::getStoreConfig("sheerid_options/settings/$key");
@@ -242,118 +96,15 @@ class SheerID_Verify_Helper_Data extends Mage_Core_Helper_Abstract
 		return !!$this->getSetting("access_token");
 	}
 
-	public function allowUploads() {
-		return $this->getBooleanSetting('allow_uploads');
-	}
-
 	public function getDefaultCampaignId() {
 		return $this->getSetting("default_campaign");
 	}
 
-	public function allowSendEmail() {
-		return $this->allowUploads() && $this->getBooleanSetting('send_email');
-	}
-
-	public function getSuccessUrl($requestId) {
-		return Mage::getUrl('SheerID/Verify/claim')."?requestId=$requestId";
-	}
-
-	public function getSiteUrl() {
-		return Mage::getUrl();
+	public function getSuccessUrl() {
+		return Mage::getUrl('SheerID/Verify/claim');
 	}
 
 	public function getService() {
 		return Mage::helper('sheerid_verify/rest')->getService();
-	}
-
-	public function getReward() {
-		$SheerID = $this->getService();
-		if ($SheerID) {
-			$rewards = $SheerID->getJson('/reward');
-			foreach ($rewards as $reward) {
-				if (array_key_exists('claimUrl', $reward->data)) {
-					return $reward;
-				}
-			}
-		}
-	}
-
-	public function createReward() {
-		$SheerID = $this->getService();
-		if ($SheerID) {
-			$data = array('claimUrl' => Mage::getUrl('SheerID/Verify/claim'));
-			$data['name'] = 'Magento';
-			$reward = json_decode($SheerID->post('/reward', $data));
-			return $reward;
-		}
-	}
-
-	public function getEmailNotifier() {
-		$SheerID = Mage::helper('sheerid_verify/rest')->getService();
-		if ($SheerID) {
-			$notifiers = $SheerID->getJson('/notifier', array('tag' => self::$NOTIFIER_TAG));
-			if (count($notifiers)) {
-				return $notifiers[0];
-			}
-		}
-	}
-
-	public function addEmailNotifier() {
-		$SheerID = Mage::helper('sheerid_verify/rest')->getService();
-		if ($SheerID) {
-			$config = array(
-				'type' => 'EMAIL',
-				'emailFromAddress' => 'Verify@SheerID.com',
-				'emailFromName' => $this->getSetting('email_from_name'),
-				'successEmailSubject' => $this->__('Successful Verification'),
-				'successEmail' => $this->__('Use the following URL to claim your offer: %successUrl%'),
-				'failureEmailSubject' => $this->__('Additional Information Required'),
-				'failureEmail' => $this->__('Unable to verify for the following reasons: %errorblock%, please try again.'),
-				'tag' => self::$NOTIFIER_TAG
-			);
-			return json_decode($SheerID->post('/notifier', $config));
-		}
-	}
-
-	public function removeEmailNotifier() {
-		$notifier = $this->getEmailNotifier();
-		if ($notifier) {
-			$notifierId = $notifier->id;
-			$SheerID = Mage::helper('sheerid_verify/rest')->getService();
-			if ($SheerID) {
-				$SheerID->delete("/notifier/$notifierId");
-			}
-		}
-	}
-
-	public function getFields($affiliation_types=null, $org_id=0) {
-                if ($affiliation_types && is_string($affiliation_types)) {
-                        $affiliation_types = explode(',', $affiliation_types);
-                }
-		$SheerID = Mage::helper('sheerid_verify/rest')->getService();
-		$fields = $SheerID->getFields($affiliation_types);
-		if ($this->allowUploads() && array_search('EMAIL', $fields) === FALSE) {
-			$fields[] = 'EMAIL';
-		}
-		return $fields;
-	}
-
-	private function readDate($request, $field) {
-		$m = $request["$field.month"];
-		$d = $request["$field.day"];
-		$y = $request["$field.year"];
-		if ($m && $d && $y) {
-			return "$y-$m-$d";
-		}
-	}
-
-	private function filterEmptyFields($params) {
-		$data = array();
-		foreach ($params as $k => $v) {
-			if ($v) {
-				$data[$k] = $v;
-			}
-		}
-		return $data;
 	}
 }
