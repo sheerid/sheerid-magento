64d0dae7f694f09c3f02f990a30e5532224a12e2
Alex Boone <alex@sheerid.com>
2014-11-25 10:34:23 -0500

    Separate tokens for sandbox and prod, fix defaults

--- app/code/SheerID/Verify/Helper/Rest.php
+++ app/code/SheerID/Verify/Helper/Rest.php
@@ -5,11 +5,12 @@ require_once ($ExternalLibPath);
 
 class SheerID_Verify_Helper_Rest extends Mage_Core_Helper_Abstract {
 	function getService() {
-		$token = Mage::getStoreConfig('sheerid_options/settings/access_token');
+		$sandbox = Mage::getStoreConfig('sheerid_options/settings/sandbox') == "1";
+		$prefix = $sandbox ? 'sandbox_' : '';
+		$token = Mage::getStoreConfig("sheerid_options/settings/${prefix}access_token");
 		if (!$token) {
 			return null;
 		}
-		$sandbox = Mage::getStoreConfig('sheerid_options/settings/sandbox') == "1";
 		return new SheerID($token, $sandbox ? SHEERID_ENDPOINT_SANDBOX : SHEERID_ENDPOINT_PRODUCTION);
 	}
 }
